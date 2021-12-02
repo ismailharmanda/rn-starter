bf09de1e03ffc031ec94563f6c89bbfddd817c6f
ismail harmanda <iharmanda@hotmail.com>
2021-12-02 22:52:58 +0300

    some basic rules' tests

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const ComponentScreen = () => {
-  return <Text style={styles.textStyle}>This is the components screen</Text>;
+  const greeting = <Text>Hello !</Text>;
+  return (
+    <View>
+      <Text style={styles.textStyle}>This is the components screen</Text>
+      {greeting}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
