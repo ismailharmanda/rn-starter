c1f2ff40e9cdfeebc854f29b0326f8219d593e20
ismail harmanda <iharmanda@hotmail.com>
2021-12-05 22:28:37 +0300

    solving "key" issue by react-nativious way

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,16 +3,16 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend #1" },
-    { name: "Friend #2" },
-    { name: "Friend #3" },
-    { name: "Friend #4" },
-    { name: "Friend #5" },
-    { name: "Friend #6" },
-    { name: "Friend #7" },
-    { name: "Friend #8" },
-    { name: "Friend #9" },
-    { name: "Friend #10" },
+    { name: "Friend #1", key: "1" },
+    { name: "Friend #2", key: "2" },
+    { name: "Friend #3", key: "3" },
+    { name: "Friend #4", key: "4" },
+    { name: "Friend #5", key: "5" },
+    { name: "Friend #6", key: "6" },
+    { name: "Friend #7", key: "7" },
+    { name: "Friend #8", key: "8" },
+    { name: "Friend #9", key: "9" },
+    { name: "Friend #10", key: "10" },
   ];
   //   const renderedList = friends.map((item) => (
   //     <Text key={item.name}>{item.name}</Text>
@@ -20,7 +20,7 @@ const ListScreen = () => {
   return (
     <FlatList
       data={friends}
-      renderItem={({ item, index }) => <Text key={index}>{item.name}</Text>}
+      renderItem={({ item, index }) => <Text>{item.name}</Text>}
     />
   );
 };
