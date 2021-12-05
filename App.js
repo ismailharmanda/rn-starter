ae7659c6b22f290264fdc664b746a6b64dcbe2ba
ismail harmanda <iharmanda@hotmail.com>
2021-12-05 20:38:20 +0300

    create a List screen with same rules in order to work on routes

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentScreen from "./src/screens/ComponentScreen";
+import ListScreen from "./src/screens/ListScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponentScreen,
+    List: ListScreen,
   },
   {
-    initialRouteName: "Components",
+    initialRouteName: "List",
     defaultNavigationOptions: {
       title: "App",
     },
