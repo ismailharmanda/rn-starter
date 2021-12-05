dc13a5a0c41475690e23ee4e46a92a6118866ad5
ismail harmanda <iharmanda@hotmail.com>
2021-12-05 23:18:00 +0300

    render two lists one after another

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -14,19 +14,41 @@ const ListScreen = () => {
     { name: "Friend #9" },
     { name: "Friend #10" },
   ];
+  const myFriends = [
+    { name: "Friend 1", age: 20 },
+    { name: "Friend 2", age: 45 },
+    { name: "Friend 3", age: 32 },
+    { name: "Friend 4", age: 27 },
+    { name: "Friend 5", age: 54 },
+    { name: "Friend 6", age: 30 },
+    { name: "Friend 7", age: 29 },
+  ];
   //   const renderedList = friends.map((item) => (
   //     <Text key={item.name}>{item.name}</Text>
   //   ));
   return (
-    <FlatList
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      keyExtractor={(item, index) => index}
-      data={friends}
-      renderItem={({ item, index }) => (
-        <Text style={styles.textStyle}>{item.name}</Text>
-      )}
-    />
+    <>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item, index) => index}
+        data={friends}
+        renderItem={({ item, index }) => {
+          return <Text>{item.name}</Text>;
+        }}
+      />
+      <FlatList
+        keyExtractor={(item, index) => item.name}
+        data={myFriends}
+        renderItem={({ item, index }) => {
+          return (
+            <Text>
+              Name:{item.name} ---- Age:{item.age}
+            </Text>
+          );
+        }}
+      />
+    </>
   );
 };
 
