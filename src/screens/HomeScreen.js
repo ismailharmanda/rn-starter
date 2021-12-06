38cbf803b80db6f2da66428b0df4ebc43ff2bf59
ismail harmanda <iharmanda@hotmail.com>
2021-12-06 21:51:47 +0300

    create a button and observe onPress (like onClick) action

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there!</Text>;
+  const buttonText = "Go to Components Demo";
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      <Button
+        onPress={() => console.log("Button pressed")}
+        title={buttonText}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
